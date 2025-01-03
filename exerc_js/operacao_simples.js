70d39682c35959ddccde0a93b78e8aef77cc4d52
Rayane Souza <rayanecoelho1504@gmail.com>
2025-01-02 21:02:29 -0300

    operacao_simples.js

---
+++ exerc_js/operacao_simples.js
@@ -0,0 +1,11 @@
+let num1 = 5, num2 = 5; 
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2;
+
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Divisão:", divisao);
+console.log("Multiplicação:", multiplicacao);
